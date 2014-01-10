581038d2da4429a820d102bbcc1e250acc07957c
SandeepK <sinha.sandeep01@gmail.com>
2014-01-10 20:29:36 +0530

    Added project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Step::Application.config.secret_key_base = '1970ef0493dbd92d4f53d59781a047455e43c707750380ce901818ed85e196f8f365897435fe0650199f42d091887a9895e1d2b052643feebbfcc563a9e7e07f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Step::Application.config.session_store :cookie_store, key: '_step_session'
